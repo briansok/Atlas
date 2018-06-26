ffebdbeb7f10d953be48af35dd405d044f7a9482
briansok <9071806+briansok@users.noreply.github.com>
2018-06-26 19:17:13 +0200

    Delete urls_local.py

--- atlas/atlas/urls_local.py
+++
@@ -1,8 +0,0 @@
-from . import urls
-from django.urls import path
-from kirrassoconsumer.views import LoginView, LoginCompleteView
-
-urls.urlpatterns += [
-    path('login/', LoginView.as_view(), name='auth-login'),
-    path('login-complete/', LoginCompleteView.as_view(), name='auth-login-complete'),
-]
